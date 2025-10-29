d2fe8624eda5d791ed7f514ac454abbb69dfccac
domi12s <117494918+domi12s@users.noreply.github.com>
2025-10-29 15:24:47 +0900

    🚀세부 화면

--- app/src/main/java/kr/ac/kumoh/s20210181/s25w08retrofit/viewmodel/SongViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20210181/s25w08retrofit/viewmodel/SongViewModel.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kr.ac.kumoh.s20210181.s25w08retrofit.model.Song
 import kr.ac.kumoh.s20210181.s25w08retrofit.repository.SongRepository
@@ -13,11 +13,7 @@ class SongViewModel(
     private val repository: SongRepository = SongRepository()
 ) : ViewModel() {
     private val _songList = MutableStateFlow<List<Song>>(emptyList())
-    // 코드가 간결해서 좋음
-    val songList: StateFlow<List<Song>> = _songList
-    // 이렇게 써도 됨
-    // val songList = _songList.asStateFlow()
-// 참조 변수가 추가되는 것을 막을 수 있음
+    val songList = _songList.asStateFlow()
 //    val songList: StateFlow<List<Song>>
 //        get() = _songList
 
@@ -30,10 +26,12 @@ class SongViewModel(
             try {
                 val response = repository.getSongs()
                 _songList.value = response
-                Log.i("fetchSongs()", response.toString())
+                //Log.i("fetchSongs()", response.toString())
             } catch(e: Exception) {
                 Log.e("fetchSongs()", e.toString())
             }
         }
     }
+
+    fun findSong(id: String) = _songList.value.find { it.id == id }
 }
